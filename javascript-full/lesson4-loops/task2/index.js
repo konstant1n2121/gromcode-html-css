c78f6fc8b28375ef59cc02447831d0084ba45ff0
konstantin <kostel1995@gmail.com>
2022-01-20 19:37:44 +0200

    tasks

--- javascript-full/lesson4-loops/task2/index.js
+++ javascript-full/lesson4-loops/task2/index.js
@@ -1,15 +1,6 @@
-const defaultUserName = 'anonymus';
-const userName = 'John';
-const anotherUser = null;
-const isAdmin = false;
-const message = 'text';
-const anotherMessage = 'text as well';
-const isLoggedIn = Boolean(userName);
-
-/* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser || defaultUserName;
-const hasAccess = isAdmin || isLoggedIn;
-const isTruthy = message != anotherMessage;
-const isFalsy = !isLoggedIn;
-const isNotTrue = typeof message === 'number';
-const isTrue = typeof message != 'boolean';
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + ' x ' + j + ' = ' + i * j);
+  }
+  console.log('=> ' + i + ' <=');
+}
