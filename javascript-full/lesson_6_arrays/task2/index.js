5c25299e80730e55c524446d6212ab74849b6409
konstantin <kostel1995@gmail.com>
2022-02-02 13:55:58 +0200

    6

---
+++ javascript-full/lesson_6_arrays/task2/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function getSum(arr) {
+ 
+
+// examples
+getSum([1, 10, -10, 4]); // ==> 5
+getSum([1]); // ==> 1
+getSum([-8, 8]); // ==> 0
+getSum(10, 12, 14); // ==> null
