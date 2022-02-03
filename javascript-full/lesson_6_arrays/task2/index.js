a97b132f5482e335dd6f7ad98f7ad5eb765c1c34
konstantin <kostel1995@gmail.com>
2022-02-03 20:55:34 +0200

    123

--- javascript-full/lesson_6_arrays/task2/index.js
+++ javascript-full/lesson_6_arrays/task2/index.js
@@ -5,10 +5,18 @@
  * @return {number}
  */
 function getSum(arr) {
- 
+  let sumOfNUmbers = 0;
 
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    sumOfNUmbers += arr[i];
+  }
+  return sumOfNUmbers;
+}
 // examples
-getSum([1, 10, -10, 4]); // ==> 5
-getSum([1]); // ==> 1
-getSum([-8, 8]); // ==> 0
-getSum(10, 12, 14); // ==> null
+console.log(getSum([6, 10, -10, 4])); // ==> 5
+console.log(getSum([1])); // ==> 1
+console.log(getSum([-8, 8])); // ==> 0
+console.log(getSum(10, 12, 14)); // ==> null
